bf6576550258ae78c42ee306ce25882e1f9fe69d
tesfaye1994my21 <woinanteneh@gmail.com>
2023-01-29 10:32:59 -0800

    update1

--- src/java/AdminUserView.java
+++ src/java/AdminUserView.java
@@ -1,13 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author WIN- 10
- */
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+@ManagedBean
+
+@SessionScoped
 public class AdminUserView {
-    
+
+    public List<UserRegisterJ> getUserList() {
+        List<UserRegisterJ> list = new ArrayList<>();
+        try {
+        
+            DBConnection dbcon = new DBConnection();
+            Connection con = dbcon.connMethod();
+            ResultSet rs = con.createStatement().executeQuery("select * from USERDATA");
+            while (rs.next()) {
+                UserRegisterJ admin = new UserRegisterJ();
+                admin.setPassword(rs.getString(1));
+                admin.setUserName(rs.getString(2));
+                admin.setEmail(rs.getString(3));
+                admin.setAddress(rs.getString(4));
+                admin.setMobileNumber(rs.getString(5));
+                
+                
+                
+                //course[1]=rs.getString("COURSE");
+                //usr.setCourse(course);
+                list.add(admin);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("error");
+            
+        }
+        return list;
+    }
+
 }
